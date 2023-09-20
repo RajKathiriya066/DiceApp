057f1ee8bcfb47169cd46cb56fd50de46f153433
RK066 <rajkathiriya66@gmail.com>
2023-09-21 04:28:03 +0530

    Added sound and shaking animation for rolling dice

--- app/src/main/java/com/practical/diceapp/MainActivity.java
+++ app/src/main/java/com/practical/diceapp/MainActivity.java
@@ -2,11 +2,14 @@ package com.practical.diceapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 
 import java.util.Random;
 
@@ -21,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         ImageView d1=findViewById(R.id.d1);
         ImageView d2=findViewById(R.id.d2);
 
+        MediaPlayer mp=MediaPlayer.create(this,R.raw.dice_sound);
+
         int[] diceNo={R.drawable.dice1,R.drawable.dice2,R.drawable.dice3,R.drawable.dice4,R.drawable.dice5,R.drawable.dice6};
 
         btn.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +37,17 @@ public class MainActivity extends AppCompatActivity {
 
                 d1.setImageResource(diceNo[generate1]);
                 d2.setImageResource(diceNo[generate2]);
+
+                YoYo.with(Techniques.Shake)
+                        .duration(400)
+                        .repeat(0)
+                        .playOn(d1);
+                YoYo.with(Techniques.Shake)
+                        .duration(600)
+                        .repeat(0)
+                        .playOn(d2);
+
+                mp.start();
             }
         });
     }
